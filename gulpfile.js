fb6c4dfbb02f3e854990db8347194247a3c13799
MuriloBSD <murilo@la804.com.br>
2018-05-01 14:44:54 -0300

    add stroke icon and email information

--- gulpfile.js
+++ gulpfile.js
@@ -80,8 +80,8 @@ gulp.task('images', () => {
 
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/webfonts')));
+    .concat(['app/fonts/**/*', 'bower_components/linearicons/dist/web-font/fonts/**/*']))
+    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
 });
 
 gulp.task('extras', () => {
