10145820e3c9e81088e28760f76772fd4419e922
murilobsd <murilo@lab804.com.br>
2018-05-01 22:57:27 -0300

    create form and custom colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,42 @@
-console.log('\'Allo \'Allo!');
+/*
+Hidden elements
+*/
+function hiddenInputs(selector) {
+    $(selector).each(function(){
+        $(this).hide();
+    });
+}
+
+/*
+Show elements
+*/
+function showInputs(selector) {
+    $(selector).each(function(){
+        $(this).show();
+        $(this).addClass('animated fadeIn');
+    });
+}
+
+function changeAccountType(selector) {
+
+    $("input[name='account_type']").on("click", function() {
+        if ($("input#account_patreon").is(":checked")) {
+            if(!$("input#email").val()) { 
+                $("input#email").attr("placeholder", "Email registered in patreon");
+            }
+            hiddenInputs(selector);
+        } else {
+            showInputs(selector);
+            if(!$("input#email").val()) { 
+                $("input#email").attr("placeholder", "Your email");
+            }
+        }
+    });
+}
+
+$(document).ready(function() {
+    // hidden default;
+    hiddenInputs(".account_free");
+    changeAccountType(".account_free");
+});
+
