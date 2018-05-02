6bd14997fc5f858adec13a99ded900720060c083
murilobsd <murilo@lab804.com.br>
2018-05-02 17:28:28 -0300

    form request apikey

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -36,7 +36,37 @@ function changeAccountType(selector) {
 
 $(document).ready(function() {
     // hidden default;
+    $("#form-response").show();
     hiddenInputs('.account_free');
     changeAccountType('.account_free');
+
+    $("#form-apikey").submit(function(e) {
+        e.preventDefault();
+        var fullname = $("#fullname").val(),
+            email = $("#email").val(),
+            account_type = $("input[name='account_type']:checked").val(),
+            use = $("#use").val();
+        
+        $.ajax({
+            type: "POST",
+            url: 'https://api.brainlamp.io/v1/apike',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'fullname': fullname,
+                'email': email,
+                'use': use,
+                'account_type': account_type
+            }),
+            success: function(res){
+                console.log(res);
+                console.log(res.data);
+                $('#form-response').html('<div class="alert alert-success rounded-0" role="alert"><strong>Please!</strong> You successfully read this important alert message.</div>');
+            },
+            error: function(res){
+                $('#form-response').html('<div class="alert alert-danger rounded-0" role="alert"><strong>Please!</strong> You successfully read this important alert message.</div>');
+            }
+        });
+
+    })
 });
 
