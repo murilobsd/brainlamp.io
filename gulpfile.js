d9ace107d683640f2166832cfcb4a95594aa49bc
murilobsd <murilo@lab804.com.br>
2018-05-05 18:12:37 -0300

    chage dest

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('images', () => {
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
     .concat(['app/fonts/**/*', 'bower_components/linearicons/dist/web-font/fonts/**/*']))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
+    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/webfonts')));
 });
 
 gulp.task('extras', () => {
